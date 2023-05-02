ec3c367c7db97c7b6d58713500016158bf50d338
l0umon <mont.gonzalez12@gmail.com>
2023-05-02 19:39:38 -0400

    CuentaDeAhorro

--- Unidad7/CuentaDeAhorros.java
+++ Unidad7/CuentaDeAhorros.java
@@ -6,5 +6,30 @@ package Unidad7;
  * @author l0umon
  */
 public class CuentaDeAhorros {
+    private static double tasalnteresAnual;
+    private double saldoAhorros;
+
+    public CuentaDeAhorros() {
+    }
+
+    public static double getTasalnteresAnual() {
+        return tasalnteresAnual;
+    }
+
+    public static void setTasalnteresAnual(double tasalnteresAnual) {
+        CuentaDeAhorros.tasalnteresAnual = tasalnteresAnual;
+    }
+
+    public double getSaldoAhorros() {
+        return saldoAhorros;
+    }
+
+    public void setSaldoAhorros(double saldoAhorros) {
+        this.saldoAhorros = saldoAhorros;
+    }
+    
+    
+    
+    
     
 }
